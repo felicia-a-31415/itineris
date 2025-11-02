5921480fb70373f4faea45b8cc29f04f2a15915d
Lela Star <lelastar8888@gmail.com>
2025-11-02 11:45:08 -0500

    Refactor App component to include Nav component and adjust styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './styles/globals.css'
+import Nav from './components/Nav'
+
 import { Routes, Route } from 'react-router-dom'
 import Bienvenue from './pages/Bienvenue'
 import TableauDeBord from './pages/TableauDeBord'
@@ -10,11 +12,14 @@ import Conseils from './pages/Conseils'
 import NotesRapides from './pages/NotesRapides'
 import Assistant from './pages/Assistant'
 import Erreur from './pages/Erreur'
-import Nav from './components/Nav'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#F5F1E8]">
+    <div className="min-h-screen bg-[#F5F1E8] text-[#2C2C2C]">
+      {/* Navigation bar at the top */}
+      <Nav />
+
+      {/* Routes define the different pages */}
       <Routes>
         <Route path="/" element={<Bienvenue />} />
         <Route path="/tableaudebord" element={<TableauDeBord />} />
